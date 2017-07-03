06cb99decddfa662d792ae30da6e9fc38c4eaac5
Jake <jakereck@gmail.com>
2017-07-02 17:06:44 -0700

    Expanded
    Fixed the text file, and added more functionality to the regex

--- regprac.py
+++ regprac.py
@@ -7,5 +7,12 @@ text_file.close()
 first_name = r'Jake'
 last_name = r'Reck'
 
-print(re.match(first_name, data))
-print(re.search(last_name, data))
+print(re.match(last_name, data))
+print(re.search(first_name, data))
+
+#FIND NAMES
+print(re.findall(r'\w*, \w+', data))
+#FIND PHONE NUMBERS
+print(re.findall(r'\(?\d{3}\)?-?\s?\d{3}-?\s?\d{4}', data))
+#FIND EMAIL ADDRESSES
+print(re.findall(r'[-\w\d+.]+@[-\w\d.]+', data))
